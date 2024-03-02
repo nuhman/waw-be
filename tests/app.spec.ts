34a3709b20c8a07d66919eac9e807ea0d68762ff
Muhammed Nuhman <nuhman7@outlook.com>
2024-03-03 02:56:54 +0530

    Integrate Japa for testing

---
+++ tests/app.spec.ts
@@ -0,0 +1,29 @@
+import { FastifyInstance } from "fastify";
+import { test } from "@japa/runner";
+import { build } from "../app.js";
+
+test.group("Application Setup", (group) => {
+  let app: FastifyInstance<any> | null = null;
+  group.setup(() => {
+    app = build({ logger: true });
+  });
+  group.teardown(async () => {
+    if (app) {
+      await app.close();
+    }
+  });
+
+  test("Health Check Endpoint Responds Successfully", async (t) => {
+    const response = await app?.inject({
+      method: "GET",
+      url: "/healthcheck",
+    });
+
+    //const response = await t.client.get("/healthcheck"); //If testing against live server
+    t.assert.equal(
+      response?.statusCode,
+      200,
+      "Expected status code 200 for health check"
+    );
+  });
+});
